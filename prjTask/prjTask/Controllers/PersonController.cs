11b5148f000cf0b5a2a34770acd8e66565e3ad86
INOXOFT\kayukov_v <victor71571@gmail.com>
2018-02-23 15:01:34 +0200

    add new controller

---
+++ prjTask/prjTask/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using prjTask.Models;
+
+namespace prjTask.Controllers
+{
+    public class PersonController : ApiController
+    {
+        PersonContext db = new PersonContext();
+        [Route("api/Person/GetAllPersons")]
+        public IEnumerable<Person> GetAllPersons()
+        {
+            return db.Persons.ToList();
+        }
+    }
+}
